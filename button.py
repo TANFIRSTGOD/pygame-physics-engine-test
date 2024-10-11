0761483c9eeee8f5e67f3b083f68146a732b467e
tan05 <tan.tantham.tan@gmail.com>
2024-10-11 20:22:21 +0700

    button class

--- button.py
+++ button.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Button:
-    def __init__(self, x, y, width, height, text, text_color, font, color, hover_color, action) -> None:   
+    def __init__(self, x, y, width, height, text, text_color, color, hover_color, primary_color, font_size) -> None:   
         self.rect = pygame.Rect(x, y, width, height)
         self.x = x
         self.y = y
@@ -9,14 +9,19 @@ class Button:
         self.height = height
         self.text = text
         self.text_color = text_color
-        self.font = font
+        self.font = pygame.font.Font(None, font_size)
         self.color = color
         self.hover_color = hover_color
+        self.primary_color = primary_color
 
         self.text_surface = self.font.render(self.text, True, text_color)
         self.text_rect = self.text_surface.get_rect(center=self.rect.center)
 
     def draw(self, screen):
+        if self.rect.collidepoint(pygame.mouse.get_pos()):
+            self.color = self.hover_color
+        else:
+            self.color = self.primary_color
         pygame.draw.rect(screen, self.color, self.rect)
         screen.blit(self.text_surface, self.text_rect)
 
--- main.py
+++ main.py
@@ -5,13 +5,14 @@ pygame.init()
 
 BACKGROUND_COLOR = (0,0,0)
 PRIMARY_COLOR = (255,255,255)
-
-DEFAULT_FONT = pygame.font.Font(None, 36)
+BUTTON_HOVER_COLOR = (225,225,225)
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Pygame physics playground test")
+
+spawnButton = button.Button(x=725, y=25, width=50, height=50, text="Spawn", text_color=BACKGROUND_COLOR, color=PRIMARY_COLOR, hover_color=BUTTON_HOVER_COLOR, primary_color=PRIMARY_COLOR, font_size=16)
 
 running = True
 
@@ -25,7 +26,7 @@ while running:
 
     screen.fill(BACKGROUND_COLOR)
 
-
+    spawnButton.draw(screen)
 
     pygame.display.flip()
 
